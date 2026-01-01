b588530697307d81e373fd0b1f8081b56f5ed146
student-manokamna <aroramoney473@gmail.com>
2026-01-01 10:51:28 +0530

    makeit

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,6 +1,7 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import {  indexRepo } from "../../../inngest/functions/index";
+import { generateReview } from "@/inngest/functions/review";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
@@ -8,6 +9,7 @@ export const { GET, POST, PUT } = serve({
   functions: [
     /* your functions will be passed here later! */
     // helloWorld
-    indexRepo
+    indexRepo,
+    generateReview
   ],
 });
---
+++ app/helloss/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HellossPage = () => {
+  return (
+    <div>
+      kaise ho !!
+    </div>
+  )
+}
+
+export default HellossPage
